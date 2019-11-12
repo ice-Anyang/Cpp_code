4e481e5aa151301ab7fa889dd28ed9c02f6b026b
ice_Anyang <1461090570@qq.com>
2019-11-12 22:53:03 +0800

    c++代码

---
+++ Pro_day_6/Pro_day_6/test.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+/*
+//int Max(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
+//double Max(double a, double b)
+//{
+//	return a > b ? a : b;
+//}
+//char Max(char a, char b)
+//{
+//	return a > b ? a : b;
+//}
+// º¯ÊýÄ£°å
+template<typename Type1, typename Type2>
+
+Type1 Max(Type1 a, Type2 b)
+{
+	return a > b ? a : b;
+}
+
+int main()
+{
+	cout << Max(2, 4) << endl;
+	cout << Max('a', 'b')<< endl;
+	cout << Max(2, 3.7) << endl;
+	return 0;
+}*/
