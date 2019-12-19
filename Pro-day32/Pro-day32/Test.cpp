b04db89778b5c025ba7164687d77b812dc3f2fe9
ice_Anyang <1461090570@qq.com>
2019-12-19 22:21:44 +0800

    c++代码

---
+++ Pro-day32/Pro-day32/Test.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+
+
+/*
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = 4;
+	int d = 7;
+
+	if (a > c)
+	if (b > c)
+		printf("%d", d++ + 1);
+	else
+		printf("%d", ++d + 1);
+	printf("%d\n", d);//7
+	return 0;
+}
+
+*/
+
+//int main()
+//{
+//
+//	int i = 0;
+//	for (i = 0; i < 3; ++i)
+//	{
+//		switch (i)
+//		{
+//		case 0:
+//			printf("%d", i);
+//			break;
+//		case 2:
+//			printf("%d", i);
+//			break;
+//		default:
+//			printf("%d", i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; ++i)
+//	{
+//		switch (i)
+//		{
+//		case 0:
+//			printf("%d", i);
+//		case 2:
+//			printf("%d", i);
+//		default:
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+//}
