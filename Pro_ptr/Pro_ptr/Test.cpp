01248a22c5edbea659ef882c2f7c3ea7e7e67942
ice_wangji <1461090570@qq.com>
2020-04-23 13:25:29 +0800

    智能指针

---
+++ Pro_ptr/Pro_ptr/Test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+/*
+//“Ï≥£
+int main()
+{
+	try
+	{
+		int *p = new int[500000000];
+	}
+	catch (bad_alloc &e)
+	{
+		cout << e.what() << endl;
+		return 0;
+	}
+	cout << "new OK." << endl;
+	return 0;
+}*/
